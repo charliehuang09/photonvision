e9205d67985cff725389939101ab1ec1bdeb49dc
Charlie Huang <charliehuang09@gmail.com>
2025-08-17 15:10:03 -0700

    docker
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-docker build --rm --platform=linux/arm64 -f docker/Dockerfile.build -t charliehuang09095/photonvision:latest --output photonvision --progress=plain .
+docker build --rm --platform=linux/arm64 -f docker/Dockerfile.build -t charliehuang09095/photonvision:latest --output . --progress=plain .
